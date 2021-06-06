d2b1fe9bd9686658e4111dd32f3ef4cc3dfa0869
Pulkit <pulkit_mlhtr@yahoo.com>
2021-06-06 23:56:17 +0530

    Dependency Injection collection added.

---
+++ PromotionEngine/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PromotionEngine.Dao;
+using PromotionEngine.Dao.Interface;
+using PromotionEngine.Logic;
+using PromotionEngine.Logic.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+   public static  class Startup
+    {
+        public static ServiceCollection collection = new ServiceCollection();
+
+        static Startup()
+        {
+            collection.AddScoped<IPromotionDao, PromotionDao>();            
+            collection.AddScoped<IProductDao, ProductDao>();
+            collection.AddScoped<IOrderDao, OrderDao>();
+            collection.AddScoped<IPromotionLogic, PromotionLogic>();
+            collection.AddScoped<IProductLogic, ProductLogic>();
+            collection.AddScoped<IOrderLogic, OrderLogic>();            
+        }
+    }
+}
